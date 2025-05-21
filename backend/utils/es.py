8386c66060506580976bfd7f510c1628f912d5a4
ankitadev <ankitae.dev@gmail.com>
2025-05-21 11:32:30 -0700

    chore: adding elastic search groun work

--- backend/utils/es.py
+++ backend/utils/es.py
@@ -1,2 +1,15 @@
 from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+ES_HOST = os.getenv("HOST")
+ES_USER = os.getenv("USER")
+ES_PASS = os.getenv("PASSWORD")
+
+def get_elastic():
+    return Elasticsearch(
+        ES_HOST,
+         basic_auth=(ES_USER, ES_PASS),
+         verify_certs=False,
+    )
